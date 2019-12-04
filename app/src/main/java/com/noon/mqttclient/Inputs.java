7b491bc5ad7f73882c16a55a7da072851320f842
lyin <lyin@noon.com>
2019-12-04 16:58:07 +0400

    Read demo config from a file

--- app/src/main/java/com/noon/mqttclient/Inputs.java
+++ app/src/main/java/com/noon/mqttclient/Inputs.java
@@ -1,5 +1,13 @@
 package com.noon.mqttclient;
 
+import android.content.Context;
+
+import com.google.gson.Gson;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Map;
+
 /**
  * Created by Islam Salah on 2019-08-15.
  * <p>
@@ -10,13 +18,39 @@ package com.noon.mqttclient;
 /* Google IoT */
 public class Inputs {
 
-    // Used for JWT password
-    static final String PROJECT_ID = "enter project id";
-    static final String CLIENT_PRIVATE_KEY = "enter private key";
+    String CLIENT_PRIVATE_KEY ;
+    String HOST_URL ;
+    String CLIENT_ID ;
+    String PROJECT_ID ;
+    String PUBLISH_TOPIC;
+    String RECEIVE_TOPIC;
+    String TEST_PAYLOAD;
 
-    static final String HOST_URL = "ssl://mqtt.eclipse.org:8883";
-    static final String DEVICE_ID = "enter device id";
-    static final String BROKER_LOCATION = "enter location";
-    static final String REGISTERY = "enter registry";
-    static final String CLIENT_ID = String.format("projects/%s/locations/%s/registries/%s/devices/%s", PROJECT_ID, BROKER_LOCATION, REGISTERY, DEVICE_ID);
+    public Inputs(Context context) {
+        readConnectionConfig(context);
+    }
+
+    private void readConnectionConfig(Context context) {
+        String json = null;
+        try {
+            InputStream is = context.getAssets().open("connections.json");
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+            json = new String(buffer, "UTF-8");
+            Gson gson = new Gson();
+            Map<String, String> map = gson.fromJson(json, Map.class);
+            CLIENT_ID = map.get("CLIENT_ID");
+            HOST_URL = map.get("HOST_URL");
+            CLIENT_PRIVATE_KEY = map.get("CLIENT_PRIVATE_KEY");
+            PROJECT_ID = map.get("PROJECT_ID");
+            PUBLISH_TOPIC = map.get("PUBLISH_TOPIC");
+            RECEIVE_TOPIC = map.get("RECEIVE_TOPIC");
+            RECEIVE_TOPIC = map.get("RECEIVE_TOPIC");
+            TEST_PAYLOAD = map.get("TEST_PAYLOAD");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
 }
--- app/src/main/java/com/noon/mqttclient/MainActivity.java
+++ app/src/main/java/com/noon/mqttclient/MainActivity.java
@@ -9,10 +9,8 @@ import com.noon.mqtt.MqttHelper;
 import com.noon.mqtt.MqttHelperImp;
 import com.noon.mqtt.PasswordGenerator;
 
-import static com.noon.mqttclient.Inputs.CLIENT_ID;
-import static com.noon.mqttclient.Inputs.CLIENT_PRIVATE_KEY;
-import static com.noon.mqttclient.Inputs.HOST_URL;
-import static com.noon.mqttclient.Inputs.PROJECT_ID;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,22 +24,32 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void runMqttHelperDemo() {
-        PasswordGenerator passwordGenerator = new PasswordGenerator(CLIENT_PRIVATE_KEY, PROJECT_ID);
+        Inputs inputs = new Inputs(this);
+        PasswordGenerator passwordGenerator = new PasswordGenerator(inputs.CLIENT_PRIVATE_KEY,
+                inputs.PROJECT_ID);
         MqttHelper mqttHelper = MqttHelperImp.builder()
                 .context(this)
-                .clientId(CLIENT_ID)
-                .hostUrl(HOST_URL)
+                .clientId(inputs.CLIENT_ID)
+                .hostUrl(inputs.HOST_URL)
                 .connectionUserName("unused")
                 .passwordGenerator(passwordGenerator)
                 .build();
 
         mqttHelper.connect();
         mqttHelper.addOnConnectionSuccessListener(() -> {
-            mqttHelper.subscribe("/devices/my-node-device/config");
+            mqttHelper.subscribe(inputs.RECEIVE_TOPIC);
+            new Timer().scheduleAtFixedRate(new TimerTask(){
+                @Override
+                public void run(){
+                    Log.d(TAG, "Publishing " + inputs.TEST_PAYLOAD);
+                    mqttHelper.publish(inputs.PUBLISH_TOPIC, inputs.TEST_PAYLOAD);
+                }
+            },0,15000);
             Log.d(TAG, "connection success!");
         });
         mqttHelper.addOnMessageArrivedListener((topic, message) -> {
             Log.d(TAG, topic + " " + message);
         });
+
     }
 }
